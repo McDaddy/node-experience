13b105d4a20924e86348fa61dca6a3c24ca7109f
陈韡韬 <cwt194536@alibaba-inc.com>
2020-09-10 14:32:55 +0800

    add generator

---
+++ src/promise/loop-test.js
@@ -0,0 +1,33 @@
+new Promise((resolve) => {
+    console.log(1)
+    resolve()
+  })
+  .then(() => {
+    console.log(2)
+    new Promise((resolve) => {
+        console.log(3)
+        resolve()
+    })
+      .then(() => {
+        console.log(4)
+        new Promise((resolve) => {
+            console.log(5)
+            resolve();
+          })
+          .then(() => {
+            console.log(7)
+          })
+          .then(() => {
+            console.log(9)
+          })
+      })
+      .then(() => {
+        console.log(8)
+      })
+  })
+  .then(() => {
+    console.log(6)
+  })
+// 宏任务队列 1
+// 微任务队列 2 4  8     6
+// 1 2 3 4 5
